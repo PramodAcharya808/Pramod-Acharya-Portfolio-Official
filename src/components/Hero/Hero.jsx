409549091294ebf4b159239e13441badf8f566be
Pramod_Acharya <pramodacharya808@gmail.com>
2024-04-18 00:51:15 +0530

    hero done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import "./Hero.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import Typewriter from "typewriter-effect";
+import DownloadBtn from "./DownloadBtn";
 
 const Hero = () => {
   return (
@@ -25,13 +26,15 @@ const Hero = () => {
           <FaLinkedin className="linkedin text-3xl md:text-5xl" />
         </a>
         <a
-          href="https://www.linkedin.com/in/pramodacharya/"
+          href="https://github.com/PramodAcharya808"
+          target="blank"
           className="active:primary-color text-purple-600 transition-all active:scale-75 active:text-gray-200"
         >
           <FaGithub className="linkedin text-3xl md:text-5xl" />
         </a>
         <a
-          href="#"
+          href="https://www.instagram.com/pramod____acharya/"
+          target="blank"
           className="active:primary-color text-purple-600 transition-all active:scale-75 active:text-gray-200"
         >
           <AiFillInstagram className="linkedin text-3xl md:text-5xl" />
@@ -57,51 +60,7 @@ const Hero = () => {
           />
         </span>
       </h3>
-      {/* <button className="cvDownload bg-primary-color rounded-2xl px-6 py-3 transition-all duration-300  hover:scale-110 active:scale-95">
-        Download CV
-      </button> */}
-
-      <div className="downloadbtn">
-        <button className="download-button">
-          <div className="docs">
-            <svg
-              className="css-i6dzq1"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              fill="none"
-              strokeWidth="2"
-              stroke="currentColor"
-              height="20"
-              width="20"
-              viewBox="0 0 24 24"
-            >
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line y2="13" x2="8" y1="13" x1="16"></line>
-              <line y2="17" x2="8" y1="17" x1="16"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-            </svg>{" "}
-            Download CV
-          </div>
-          <a className="download" href="https://www.google.com" target="blank">
-            <svg
-              className="css-i6dzq1"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              fill="none"
-              strokeWidth="2"
-              stroke="currentColor"
-              height="24"
-              width="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="7 10 12 15 17 10"></polyline>
-              <line y2="3" x2="12" y1="15" x1="12"></line>
-            </svg>
-          </a>
-        </button>
-      </div>
+      <DownloadBtn />
     </div>
   );
 };
