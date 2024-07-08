e0061c8fb33461e0dcabfb5f1aa11ce3fb3b520a
Pramod_Acharya <pramodacharya808@gmail.com>
2024-07-08 11:54:24 +0530

    responsive design changes of the topmate button

--- src/components/ScrollTopBtn/ScrollTop.jsx
+++ src/components/ScrollTopBtn/ScrollTop.jsx
@@ -14,7 +14,7 @@ const ScrollTop = () => {
         href="https://topmate.io/pramod_acharya?utm_source=linkedin&utm_medium=product&utm_campaign=ss"
         target="blank"
       >
-        <button className="scrollBtn fixed left-[80%] top-[90%] z-[999] flex h-8 w-32 items-center justify-center rounded-full border border-black bg-white font-bold text-black transition-all hover:scale-110 active:scale-100">
+        <button className="scrollBtn fixed left-[70%] top-[95%] z-[999] flex h-8 w-36 items-center justify-center rounded-l-full bg-[#a5079d] font-bold text-white transition-all hover:scale-110 active:scale-100 md:left-[80%] md:top-[90%] md:w-32 md:rounded-full">
           Connect 1 : 1
         </button>
       </a>
